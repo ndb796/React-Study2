eefd5de2bb9ed771bd3b7a5ffb069385f309b8e6
ndb796 <ndb796@naver.com>
2018-12-30 06:04:14 +0900

    Create React-Contact-App

---
+++ src/components/ContactInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContactInfo extends React.Component {
+    render() {
+        return (
+            <div onClick={this.props.onClick}>
+                {this.props.contact.name}
+            </div>
+        );
+    }
+}
+
+export default ContactInfo;
